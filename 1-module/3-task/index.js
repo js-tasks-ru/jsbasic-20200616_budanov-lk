7bf3c3e8b8e6ba2f097db01a6611dc2476d42555
alexander <budanov_lk@mail.ru>
2020-06-23 16:48:29 +0300

    1 модуль 3 задача решена верно

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -4,5 +4,10 @@
  * @returns {string}
  */
 function ucFirst(str) {
-  // ваш код...
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+let name = ucFirst('вася');
+let name2 = ucFirst('в');
+let emptyName = ucFirst('');
+
