013b9ddba51395ecc3c5f27bc8f45ed8596e6431
alexander <budanov_lk@mail.ru>
2020-06-28 16:13:22 +0300

    задача 4 решена

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,9 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-  // ваш код...
+  let lowString = str.toLowerCase();
+  return lowString.includes('1xbet') || lowString.includes('xxx');
 }
+checkSpam ('1XbeT now');
+checkSpam ('free xxxxx');
+checkSpam ('innocent rabbit');
