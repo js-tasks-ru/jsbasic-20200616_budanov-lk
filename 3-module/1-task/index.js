70de60f3e99bc482f6a0d7b99f5729d687a79626
alexander <budanov_lk@mail.ru>
2020-07-04 15:10:36 +0300

    Задача 1 решена

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -3,5 +3,13 @@
  * @returns {string[]}  объект
  */
 function namify(users) {
-  // ваш код...
+  let userNames = users.map( item => item.name);
+  return userNames;
 }
+let vasya = { name: 'Вася', age: 25 };
+let petya = { name: 'Петя', age: 30 };
+let masha = { name: 'Маша', age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+let names = namify(users);
