72467b45deb05a18b56db6158af6daffaeb21c64
alexander <budanov_lk@mail.ru>
2020-07-13 13:17:51 +0300

    Задача 2 решена

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -2,5 +2,10 @@
  * @param {HTMLTableElement} table
  * @return {void}
  */
-function makeDiagonalRed(table) {
+function makeDiagonalRed(parametr) {
+    let result = document.body.firstElementChild;
+    for (let i = 0; i < parametr.rows.length; i++) {
+      parametr.rows[i].cells[i].style.backgroundColor = 'red';
+    }
+    return result;
 }
