6de51918d17e0c3c88a14bc610d7d0a36ccd4dd4
alexander <budanov_lk@mail.ru>
2020-07-14 18:59:03 +0300

    Задача 3 решена

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,13 @@
  * @param {Element} table
  */
 function highlight(table) {
+    for (let i = 1; i < table.rows.length; i++) {
+        table.rows[i].cells[3].getAttribute('data-available') == 'true' ? table.rows[i].classList.add('available') : 
+        table.rows[i].cells[3].getAttribute('data-available') == 'false' ? table.rows[i].classList.add('unavailable') : 
+        table.rows[i].setAttribute('hidden', 'text') ;
+        table.rows[i].cells[2].innerHTML == 'm' ? table.rows[i].classList.add('male') : table.rows[i].classList.add('female');
+        if (table.rows[i].cells[1].innerHTML < 18) {
+          table.rows[i].style.textDecoration = 'line-through';
+        }   
+       }
 }
