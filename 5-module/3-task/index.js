98b50da4c74de4fafe8993757df34df19bf363ce
alexander <budanov_lk@mail.ru>
2020-08-03 17:11:05 +0300

    Задача 3 решена

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,18 +1,18 @@
 function initCarousel() {
   let carouselArrowRight = document.querySelector('.carousel__arrow_right');
   let carouselArrowLeft = document.querySelector('.carousel__arrow_left');
-  let elem = document.querySelector('.carousel__inner');
-  let width = elem.offsetWidth;
+  let carouselInner = document.querySelector('.carousel__inner');
+  let width = carouselInner.offsetWidth;
   let position = 0;
   carouselArrowRight.onclick = function() {
   position -= width;
-  elem.style.transform = `translateX(${position}px)`;
+  carouselInner.style.transform = `translateX(${position}px)`;
   (position == -width * 3) ?
     carouselArrowRight.style.display = 'none': carouselArrowLeft.style.display = '';
   };
  carouselArrowLeft.onclick = function() {
   position += width;
-  elem.style.transform = `translateX(${position}px)`;
+  carouselInner.style.transform = `translateX(${position}px)`;
   (position == 0) ?
   carouselArrowLeft.style.display = 'none': carouselArrowRight.style.display = '';
  };
